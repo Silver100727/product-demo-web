3b7f4a1d21e48261c819111d68c6f39391faccac
Sylvester Campbell <sylvester@imperiumapp.com>
2025-03-01 17:25:08 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,21 @@
-import { FacebookIcon, Gift, InstagramIcon, TwitterIcon } from "lucide-react";
+import { FacebookIcon, Gift, InstagramIcon, LinkedinIcon } from "lucide-react";
 import React from "react";
 import { Link } from "react-router-dom";
+
+const Whatsapp = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M16.6 14c-.2-.1-1.5-.7-1.7-.8c-.2-.1-.4-.1-.6.1c-.2.2-.6.8-.8 1c-.1.2-.3.2-.5.1c-.7-.3-1.4-.7-2-1.2c-.5-.5-1-1.1-1.4-1.7c-.1-.2 0-.4.1-.5c.1-.1.2-.3.4-.4c.1-.1.2-.3.2-.4c.1-.1.1-.3 0-.4c-.1-.1-.6-1.3-.8-1.8c-.1-.7-.3-.7-.5-.7h-.5c-.2 0-.5.2-.6.3c-.6.6-.9 1.3-.9 2.1c.1.9.4 1.8 1 2.6c1.1 1.6 2.5 2.9 4.2 3.7c.5.2.9.4 1.4.5c.5.2 1 .2 1.6.1c.7-.1 1.3-.6 1.7-1.2c.2-.4.2-.8.1-1.2l-.4-.2m2.5-9.1C15.2 1 8.9 1 5 4.9c-3.2 3.2-3.8 8.1-1.6 12L2 22l5.3-1.4c1.5.8 3.1 1.2 4.7 1.2c5.5 0 9.9-4.4 9.9-9.9c.1-2.6-1-5.1-2.8-7m-2.7 14c-1.3.8-2.8 1.3-4.4 1.3c-1.5 0-2.9-.4-4.2-1.1l-.3-.2l-3.1.8l.8-3l-.2-.3c-2.4-4-1.2-9 2.7-11.5S16.6 3.7 19 7.5c2.4 3.9 1.3 9-2.6 11.4"
+    ></path>
+  </svg>
+);
 
 const Footer = () => {
   return (
@@ -55,20 +70,33 @@ const Footer = () => {
             <h3 className="text-lg font-bold mb-4">Follow Us</h3>
             <div className="flex space-x-4">
               {[
-                { icon: <InstagramIcon className="h-6 w-6" /> },
-                { icon: <FacebookIcon className="h-6 w-6" /> },
-                { icon: <TwitterIcon className="h-6 w-6" /> },
-              ].map((item, index) => {
-                return (
-                  <a
-                    href="#"
-                    key={index}
-                    className="text-gray-400 hover:text-white"
-                  >
-                    {item.icon}
-                  </a>
-                );
-              })}
+                {
+                  icon: <InstagramIcon className="h-6 w-6" />,
+                  link: "https://www.instagram.com/rsgratitudegifts/",
+                },
+                {
+                  icon: <FacebookIcon className="h-6 w-6" />,
+                  link: "https://www.facebook.com/people/RS-Gratitude-Gifts/61573472153305/",
+                },
+                {
+                  icon: <Whatsapp className="h-6 w-6" />,
+                  link: "https://wa.me/919900123901",
+                },
+                {
+                  icon: <LinkedinIcon className="h-6 w-6" />,
+                  link: "https://www.linkedin.com/in/rs-gratitude-gifts-642524351/",
+                },
+              ].map((item, index) => (
+                <a
+                  href={item.link}
+                  key={index}
+                  className="text-gray-400 hover:text-white"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {item.icon}
+                </a>
+              ))}
             </div>
           </div>
         </div>
